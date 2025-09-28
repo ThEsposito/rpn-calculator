cc807028e43eb6c3b809519db1134f5aaaa8f361
ThEsposito <thesposito.estudos@gmail.com>
2025-09-28 20:53:49 -0300

    Criando novas exceções

---
+++ src/exceptions/VariavelNaoDefinidaException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class VariavelNaoDefinidaException extends RuntimeException {
+    public VariavelNaoDefinidaException(String message) {
+        super(message);
+    }
+}
