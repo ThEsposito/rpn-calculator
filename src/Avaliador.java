94db928232cd40cd13bc57ec899ea197b62735eb
ThEsposito <thesposito.estudos@gmail.com>
2025-09-28 17:27:16 -0300

    Avaliador - infixToPosfix + metodos utilitarios

--- src/Avaliador.java
+++ src/Avaliador.java
@@ -13,6 +13,8 @@ até que seja desempilhado o parêntese de abertura correspondente.
 • Ao final da varredura, esvazie a pilha, movendo os símbolos desempilhados para a saída.
 */
 
+import data.structures.Pilha;
+
 public class Avaliador {
     private Variaveis vars;
 
@@ -23,26 +25,67 @@ public class Avaliador {
     public Avaliador(Variaveis vars){
         this.vars = vars;
     }
-
+/*
+1. Enquanto a pilha não estiver vazia e houver no seu topo um operador com prioridade maior
+ou igual ao encontrado, desempilhe o operador e copie-o na saída;
+2. Empilhe o operador encontrado;
+ */
     public String infixToPosfix(String infix){
         String result = "";
-        for(int i = 0; i < infix.length(); i++){
+        Pilha<Character> p1 = new Pilha(infix.length());
 
+        for(int i = 0; i < infix.length(); i++){
+            char atual = Character.toUpperCase(infix.charAt(i));
+
+            if(Character.isAlphabetic(atual)){ //se achar um identificador
+                result += atual; //concatenar em resultado
+            }
+            else if(this.isOperador(atual)){
+                while(!p1.isEmpty() && isOperador(p1.top()) && getPrioridadeDoOperador(p1.top()) >= getPrioridadeDoOperador(atual)){
+                        result += p1.pop(); // desempilha operadores de maior ou igual prioridade
+                }
+                p1.push(atual); // empilha o operador atual ATENÇÃO A TESTES DE PRIORIDADES IGUAIS
+            }
+        }
+        // Desempilha o que restou
+        while (!p1.isEmpty()) {
+            result += p1.pop();
         }
 
         return result;
     }
 
+    // TODO: implementar esse método
     public Double avaliarPosfix(){
-
-
-        return result2 = 0;
+        return 0.0;
     }
 
     public Boolean validarExpressao(){ //avalia a entrada infixa
-        boolean result3;
+        boolean result = true;
+        return result;
+    }
 
+    private boolean isOperador(char c){
+        char[] operadores = {'+','-', '*','/', '^'};
 
-        return result3;
+        for(char operador : operadores) {
+            if(c == operador) return true;
+        }
+        return false;
+    }
+
+    private int getPrioridadeDoOperador(char operador) throws IllegalArgumentException {
+        if(!isOperador(operador)) throw new IllegalArgumentException("O parâmetro deve ser um operador!");
+        switch(operador){
+            case '+', '-':
+                return 1;
+
+            case '/', '*':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        throw new IllegalArgumentException("Operador inválido!");
     }
 }
